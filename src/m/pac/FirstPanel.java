cb2fbe910a75bd9fa6260a61ea8517c3f223549e
Alex <furriets@gmail.com>
2017-06-15 17:46:54 +0300

    add grafic in first panel and two field in second panel

--- src/m/pac/FirstPanel.java
+++ src/m/pac/FirstPanel.java
@@ -7,52 +7,83 @@ import java.awt.*;
 public class FirstPanel extends JPanel
 {
     private JLabel time;
-    private JLabel yourField;
-    private JLabel myField;
+    //private JLabel yourField;
+   // private JLabel myField;
     private JLabel status;
     private GridBagLayout gbl;
     private GridBagConstraints gbc;
+    private Polygon poligon;
+    int [] xCor;
+    int [] yCor;
 
     public FirstPanel(int w, int h)
     {
         setPreferredSize(new Dimension(w,h));
 
-        System.out.println(this.getSize() + "PERvaz panell");
+        gbl = new GridBagLayout();
+        setLayout(gbl);
         time = new JLabel("Time");
-        yourField = new JLabel("YourField");
-        myField = new JLabel("MyField");
+       // yourField = new JLabel("YourField");
+        //myField = new JLabel("MyField");
         status = new JLabel("Status: ");
-        add(time, BorderLayout.NORTH);
-        add(status, BorderLayout.WEST);
 
-//        gbl = new GridBagLayout();
-//        setLayout(gbl);
-//        gbc = new GridBagConstraints();
+         gbc = new GridBagConstraints(1,0,1,1,1,1,GridBagConstraints.NORTH,GridBagConstraints.NONE,
+                 new Insets(0,0,0,0),0,0);
 
-        setBackground(Color.ORANGE);
-//        setComponetStatus(status);
-//        setComponetTime(time);
-//        setComponetNull(new JLabel("Hello "));
+        gbl.setConstraints(time, gbc);
+        add(time);
+
+        gbc = new GridBagConstraints(1,1,1,1,0,0,GridBagConstraints.CENTER,GridBagConstraints.NONE,
+                new Insets(0,0,0,0),0,0);
+        gbl.setConstraints(status, gbc);
+        add(status);
+
+        //setBackground(Color.ORANGE);
+
     }
 
-//    private void setComponetStatus(Component componets)
-//    {
-//        add(componets, new GridBagConstraints(0, 0, 1, 1, 1, 1, GridBagConstraints.NORTHWEST, GridBagConstraints.HORIZONTAL,
-//                new Insets(0, 0 ,0,0 ), 0,0));
-//    }
-//
-//    private void setComponetTime(Component componets)
-//    {
-////        add(componets, new GridBagConstraints(0, 1, 1, 0, 0, 0, GridBagConstraints.NORTH, GridBagConstraints.NONE,
-////                new Insets(0, 0 ,0,0 ), 0,0));
-//
-//        add(componets, new GridBagConstraints(1, 0, 1, 1, 1, 1, GridBagConstraints.NORTHWEST, GridBagConstraints.NONE,
-//                new Insets(0, 0 ,0,0 ), 0,0));
-//    }
-//
-//    private void setComponetNull(Component componets)
-//    {
-//        add(componets, new GridBagConstraints(2, 0, 1, 1, 1, 1, GridBagConstraints.NORTHWEST, GridBagConstraints.NONE,
-//                new Insets(0, 0 ,0,0 ), 0,0));
-//    }
+    public void paint(Graphics g)
+    {
+        g.setColor(Color.orange);
+        setCoordinates(getWidth(),getHeight(),5);
+        System.out.println(getWidth()+" ===== " + getHeight());
+        g.fillRect(0,0,getWidth(),getHeight());
+        g.setColor(Color.red);
+        poligon = new Polygon(xCor,yCor,5);
+//        g.drawPolygon(poligon);
+        g.fillPolygon(poligon);
+    }
+
+
+    private void setCoordinates(int w, int h, int v)
+    {
+        xCor = new int[v];
+        yCor = new int[v];
+
+        for(int i = 0; i < v; i++)
+        {
+            if (i == 0) {
+                xCor[i] = (w / 2) - 20;
+                yCor[i] = 0;
+                System.out.println( xCor[i] + " I = 0 " + yCor[i]);
+            } else if (i == 1) {
+                xCor[i] = (w / 2) + 20;
+                yCor[i] = 0;
+                System.out.println(xCor[i] + " i == 1 " + yCor[i]);
+            } else if (i == 2) {
+                xCor[i] = (w / 2) + 60;
+                yCor[i] = h;
+                System.out.println(xCor[i] + " i == 2 " + yCor[i]);
+            } else if (i == 3) {
+                xCor[i] = (w / 2)  - 60;
+                yCor[i] = h;
+                System.out.println(xCor[i] + " i == 3 " + yCor[i]);
+            } else if (i == 4) {
+                xCor[i] = (w / 2) - 20;
+                yCor[i] = 0;
+                System.out.println(xCor[i] + " i == 4 " + yCor[i]);
+            }
+        }
+    }
+
 }
--- src/m/pac/SecondPanel.java
+++ src/m/pac/SecondPanel.java
@@ -5,29 +5,25 @@ import java.awt.*;
 
 public class SecondPanel extends JPanel
 {
-    private int x;
-    private int y;
 
     public SecondPanel(int w, int h)
     {
-        x = getWidth();
-        y = getHeight();
-
-        System.out.println(x + " ++++++++++++++++++++++++++++++ " + y);
         setPreferredSize(new Dimension(w,h ));
         setBackground(Color.RED);
     }
 
     public void paint(Graphics g)
     {
-        //super(g);
+        g.setColor(new Color(29,172,214));
+        g.fillRect(0,0,getWidth(),getHeight());
         g.setColor(Color.black);
-        System.out.println(getHeight() + " -- " + this.getSize());
-        drawFieldWar(g, 40, 20);
-        System.out.println("Method Srabotal");
+        drawFieldWar(g);
+        drawFieldWarTwo(g);
+        g.setColor(Color.red);
+        g.fillRect(getWidth() / 2 - 20, 0,40, getHeight());
     }
 
-    private void drawFieldWar(Graphics g, int wX, int hY)
+    private void drawFieldWar(Graphics g)
     {
         int w = this.getWidth();
         int h = this.getHeight();
@@ -37,7 +33,6 @@ public class SecondPanel extends JPanel
         int right = 40;
         int bottom = 20;
 
-        System.out.println(getHeight() + " -- " + this.getSize() + "ЭТО БЛОК ГРАФИКИ!");
 
         int stepX = (halfWidth - right - left) / 11;
         int stepY = (h - top - bottom) / 11;
@@ -54,4 +49,30 @@ public class SecondPanel extends JPanel
             g.drawLine(left, point, stepX * 10 + left, point);
         }
     }
+
+    private void drawFieldWarTwo(Graphics g)
+    {
+        int w = this.getWidth();
+        int h = this.getHeight();
+        int top = 20;
+        int left = 572;
+        int right = 40;
+        int bottom = 20;
+
+        int stepX = (w  - left) / 11;
+        int stepY = (h - top - bottom) / 11;
+
+        for (int i = 0; i < 11; i++ )
+        {
+            int point = (i * stepX) + left;
+            g.drawLine(point, top, point,stepY * 10 + top);
+        }
+
+        for (int i = 0; i < 11; i++ )
+        {
+            int point = (i * stepY) + top;
+            g.drawLine(left, point, stepX * 10 + left, point);
+        }
+    }
+
 }
